f93d4d18cf886944b99c035e26dc33f9bc652dd7
Rene <36115705+rene78@users.noreply.github.com>
2022-02-08 23:39:48 +0800

    first commit

---
+++ localeUtils.js
@@ -0,0 +1,16 @@
+// import * as EN from './locales/en.json';
+// import * as DE from './locales/de.json';
+
+import EN from "./locales/en.json" assert { type: "json" };
+import DE from "./locales/de.json" assert { type: "json" };
+
+export function localeString(localeString) {
+  const locale  = "DE";
+
+  switch (locale) {
+      case 'DE':
+          return DE[localeString] || EN[localeString];
+      default:
+          return EN[localeString];
+  }
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+import { localeString } from './localeUtils.js';
+
+const country1 = localeString("countries.DEU");
+document.querySelector("#country-1").innerText = country1;
