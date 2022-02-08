d846cfa18d21a430076940a8dde5161325e10272
Rene <36115705+rene78@users.noreply.github.com>
2022-02-08 23:57:08 +0800

    just a test

--- localeUtils.js
+++ localeUtils.js
@@ -1,16 +1,14 @@
-// import * as EN from './locales/en.json';
-// import * as DE from './locales/de.json';
-
 import EN from "./locales/en.json" assert { type: "json" };
 import DE from "./locales/de.json" assert { type: "json" };
 
 export function localeString(localeString) {
-  const locale  = "DE";
+  // const locale  = "DE";
 
-  switch (locale) {
-      case 'DE':
-          return DE[localeString] || EN[localeString];
-      default:
-          return EN[localeString];
-  }
+  // switch (locale) {
+  //     case 'DE':
+  //         return DE[localeString] || EN[localeString];
+  //     default:
+  //         return EN[localeString];
+  // }
+  return localeString;
 }
