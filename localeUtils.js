013572a1bc0cb2119869af3ef78a74dab770e80c
Rene <36115705+rene78@users.noreply.github.com>
2022-02-09 16:40:06 +0800

    clean-up & file separation

--- localeUtils.js
+++ localeUtils.js
@@ -1,19 +1,14 @@
-/*As of 2022-02 the import below only works for Chromium based browsers (see https://stackoverflow.com/a/68593192/5263954)*/
-// import EN from "./locales/en.json" assert { type: "json" };
-// import DE from "./locales/de.json" assert { type: "json" };
-
-const locale = loadLanguageFile("de");
-const EN = loadLanguageFile("en");
+const locale = await loadLanguageFile("de");
+const EN = await loadLanguageFile("en");
 
 async function loadLanguageFile(locale) {
   const linkToLanguageFile = "./locales/" + locale + ".json";
   const response = await fetch(linkToLanguageFile);
   const languageFile = await response.json();
-  console.log(languageFile);
   return languageFile;
 }
 
 export function localeString(localeString) {
-  // console.log(locale);
+  console.log(locale);
   return locale[localeString] || EN[localeString];
 }
--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-// import { localeString } from './localeUtils.js';
-
-const locale = await loadLanguageFile("de");
-const EN = await loadLanguageFile("en");
-
-async function loadLanguageFile(locale) {
-  const linkToLanguageFile = "./locales/" + locale + ".json";
-  const response = await fetch(linkToLanguageFile);
-  const languageFile = await response.json();
-  console.log(languageFile);
-  return languageFile;
-}
-
-function localeString(localeString) {
-  // console.log(locale);
-  return locale[localeString] || EN[localeString];
-}
+import { localeString } from './localeUtils.js';
 
 const country1 = localeString("countries.DEU")
 document.querySelector("#country-1").innerText = country1;
