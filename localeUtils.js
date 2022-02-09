1ff3fca2c916c110b779b25ade4c2690b7bb42f1
Rene <36115705+rene78@users.noreply.github.com>
2022-02-09 12:19:14 +0800

    test

--- localeUtils.js
+++ localeUtils.js
@@ -1,13 +1,24 @@
+/*As of 2022-02 the import below only works for Chromium based browsers (see https://stackoverflow.com/a/68593192/5263954)*/
 import EN from "./locales/en.json" assert { type: "json" };
 import DE from "./locales/de.json" assert { type: "json" };
 
+const locale = "DE";
+// const languageFile =loadLanguageFile("de");
+// console.log(languageFile);
+
+async function loadLanguageFile(locale) {
+  const linkToLanguageFile = "./locales/" + locale + ".json";
+  let response = await fetch(linkToLanguageFile);
+  let languageFile = await response.json();
+  return languageFile;
+}
+
 export function localeString(localeString) {
-  const locale  = "DE";
 
   switch (locale) {
-      case 'DE':
-          return DE[localeString] || EN[localeString];
-      default:
-          return EN[localeString];
+    case 'DE':
+      return DE[localeString] || EN[localeString];
+    default:
+      return EN[localeString];
   }
 }
